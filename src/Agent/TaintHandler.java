c82d9bdc6fa689bc49814d45688f74b89e48917e
Maciej Cymbalista <maciekcymbalista@wp.pl>
2019-07-09 21:41:49 +0200

    TaintHandler impemented

--- src/Agent/TaintHandler.java
+++ src/Agent/TaintHandler.java
@@ -1,4 +1,30 @@
 package Agent;
 
+import static java.lang.System.out;
+
 public class TaintHandler {
+
+    private static void setTaint(Object s, boolean value, String className) {
+        if (s instanceof Taintable) {
+            ((Taintable) s).setTaint(value, className);
+        }
+    }
+
+    public static void taint(Object s, String className) {
+        setTaint(s, true, className);
+    }
+
+    public static void detaint(Object s) {
+        setTaint(s, false, null);
+    }
+
+    public static boolean isTainted(Object s) {
+        return s instanceof Taintable && ((Taintable) s).isTainted();
+    }
+
+    public static void checkTaint(Object s, String classname){
+        if(isTainted(s)){
+            out.println("Tainted value sent without sanitarization");
+        }
+    }
 }
